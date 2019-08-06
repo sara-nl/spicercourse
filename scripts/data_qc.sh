fc19b2a7139f0a381feb8d0356e22587e391b08f
Maithili Kalamkar <maithili86@gmail.com>
2019-08-06 15:11:35 +0200

    Create data_qc.sh

---
+++ scripts/data_qc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+#if you are a data manager, uncomment the following line (remove the #)
+#ecolipath=/project/spidercourse/Data/ecoli-analysis
+
+#if you are a regular user, uncomment the following line (remove the #)
+#ecolipath=$HOME/ecoli-analysis
+
+mkdir -p $ecolipath/data/fastqc_untrimmed_reads
+
+cd $ecolipath/data/fastqc_untrimmed_reads/
+
+echo "Running FastQC ..."
+fastqc $ecolipath/data/untrimmed_fastq/*.fastq* -o ./ 
+
+cd $ecolipath/data/untrimmed_fastq
+cp /project/spidercourse/Software/ecoli-analysis-software/miniconda2/pkgs/trimmomatic-0.38-0/share/trimmomatic-0.38-0/adapters/NexteraPE-PE.fa .
+echo "Running trimmomatic"
+for infile in *_1.fastq.gz
+do
+   base=$(basename ${infile} _1.fastq.gz)
+   trimmomatic PE ${infile} ${base}_2.fastq.gz \
+               ${base}_1.trim.fastq.gz ${base}_1un.trim.fastq.gz \
+               ${base}_2.trim.fastq.gz ${base}_2un.trim.fastq.gz \
+               SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15 
+done
+
+mkdir $ecolipath/data/trimmed_fastq	
+mv *.trim* $ecolipath/data/trimmed_fastq
+
+echo "done"
