75c0f13e54593894385616ea54e7722252e13710
Maithili Kalamkar <maithili86@gmail.com>
2019-08-13 15:29:22 +0200

    Update my-first-job.sh

--- scripts/my-first-job.sh
+++ scripts/my-first-job.sh
@@ -11,7 +11,7 @@ echo "The status of the worker nodes is as displayed below:"
 sinfo
 echo ""
 echo "The current running jobs are listed below:"
-squeue
+squeue -u $USER
 echo ""
 echo "You just ran your first job on" $HOSTNAME " with a job ID " $SLURM_JOBID
 echo ""
