63334ea2363b74c71886101e000f7e7790cb52f0
Maithili Kalamkar <maithili86@gmail.com>
2019-08-14 17:00:00 +0200

    Update job-submit-variant-calling-dcache-adv.sh

--- scripts/job-submit-variant-calling-dcache-adv.sh
+++ scripts/job-submit-variant-calling-dcache-adv.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH --constraint=skylake
 
+mkdir "$TMPDIR"/var-calling
+cd "$TMPDIR"/var-calling
+
+cp $HOME/ecoli-analysis-dcache/run-variant-calling-dcache-adv.sh .
+
+export PATH="/project/spidercourse/Software/ecoli-analysis-software/miniconda2/bin:$PATH"
+
+bash run-variant-calling-dcache-adv.sh 
