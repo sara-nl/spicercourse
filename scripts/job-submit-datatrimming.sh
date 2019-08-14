e8163669e281f4f625abf8619bf002d6b77bc4ce
Maithili Kalamkar <maithili86@gmail.com>
2019-08-14 15:39:46 +0200

    Update job-submit-datatrimming.sh

--- scripts/job-submit-datatrimming.sh
+++ scripts/job-submit-datatrimming.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH -c 1
 #SBATCH --constraint=skylake
-#SBATCH --reservation=spidercourse_2
 
 #As data manager uncomment the following line
 #bash /project/spidercourse/Data/ecoli-analysis/data_qc.sh 
