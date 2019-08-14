ca8517d0efcf9c815eef98745ca66706c3e3b569
Maithili Kalamkar <maithili86@gmail.com>
2019-08-14 12:54:13 +0200

    Update job-submit-variant-calling-tmpdir.sh

--- scripts/job-submit-variant-calling-tmpdir.sh
+++ scripts/job-submit-variant-calling-tmpdir.sh
@@ -6,6 +6,6 @@
 mkdir "$TMPDIR"/var-calling
 cd "$TMPDIR"/var-calling
 
-cp $HOME/ecoli-analysis/run-variant-calling-tmpdir.sh .
+cp $HOME/ecoli-analysis-tmpdir/run-variant-calling-tmpdir.sh .
 
 time bash run-variant-calling-tmpdir.sh 
