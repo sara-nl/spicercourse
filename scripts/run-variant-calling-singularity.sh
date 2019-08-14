6e097adb106b202b28d45f7cba2a77146f92a480
Maithili Kalamkar <maithili86@gmail.com>
2019-08-14 13:26:53 +0200

    Update run-variant-calling-singularity.sh

--- scripts/run-variant-calling-singularity.sh
+++ scripts/run-variant-calling-singularity.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
 set -e
-ecolipath=$HOME/ecoli-analysis
+ecolipath=$HOME/ecoli-analysis-container
 
+mkdir -p data/ref_genome
+cp /project/spidercourse/Data/ecoli-analysis/data/ref_genome/ecoli_rel606.fasta data/ref_genome/
+ls data/ref_genome
+
+mkdir data/trimmed_fastq_small
+cp /project/spidercourse/Data/ecoli-analysis/data/trimmed_fastq_small/*fastq data/trimmed_fastq_small/
+ls data/trimmed_fastq_small
+
+mkdir results
 cd $ecolipath/results
 
 genome=$ecolipath/data/ref_genome/ecoli_rel606.fasta
 
-singularity exec $HOME/ecoli-analysis/elixir-singularity.sif bwa index $genome
+singularity exec $ecolipath/elixir-singularity.sif bwa index $genome
 
 mkdir -p sam bam bcf vcf
 
@@ -27,21 +35,21 @@ for fq1 in $ecolipath/data/trimmed_fastq_small/*_1.trim.sub.fastq
     final_variants=$ecolipath/results/vcf/${base}_final_variants.vcf 
     
     #Align reads to reference genome
-    singularity exec $HOME/ecoli-analysis/elixir-singularity.sif bwa mem $genome $fq1 $fq2 > $sam
+    singularity exec $ecolipath/elixir-singularity.sif bwa mem $genome $fq1 $fq2 > $sam
     
     #Convert from sam to bam format
-    singularity exec $HOME/ecoli-analysis/elixir-singularity.sif samtools view -S -b $sam > $bam
+    singularity exec $ecolipath/elixir-singularity.sif samtools view -S -b $sam > $bam
 
     #Sort the bam files    
-    singularity exec $HOME/ecoli-analysis/elixir-singularity.sif samtools sort -o $sorted_bam $bam 
+    singularity exec $ecolipath/elixir-singularity.sif samtools sort -o $sorted_bam $bam 
     
     #Calculate the read coverage of positions in the genome
-    singularity exec $HOME/ecoli-analysis/elixir-singularity.sif bcftools mpileup -O b -o $raw_bcf -f $genome $sorted_bam
+    singularity exec $ecolipath/elixir-singularity.sif bcftools mpileup -O b -o $raw_bcf -f $genome $sorted_bam
     
     #Detect the single nucleotide polymorphisms (SNPs)
-    singularity exec $HOME/ecoli-analysis/elixir-singularity.sif bcftools call --ploidy 1 -m -v -o $variants $raw_bcf 
+    singularity exec $ecolipath/elixir-singularity.sif bcftools call --ploidy 1 -m -v -o $variants $raw_bcf 
     
     #Filter the SNPs for the final output in VCF format
-    singularity exec $HOME/ecoli-analysis/elixir-singularity.sif vcfutils.pl varFilter $variants > $final_variants
+    singularity exec $ecolipath/elixir-singularity.sif vcfutils.pl varFilter $variants > $final_variants
    
     done
