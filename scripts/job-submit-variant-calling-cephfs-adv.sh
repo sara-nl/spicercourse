6751c370d476803434a41bd827d97a04e7d0e0dd
Maithili Kalamkar <maithili86@gmail.com>
2019-08-27 16:12:41 +0200

    Update job-submit-variant-calling-cephfs-adv.sh

--- scripts/job-submit-variant-calling-cephfs-adv.sh
+++ scripts/job-submit-variant-calling-cephfs-adv.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-#SBATCH -c 1
-#SBATCH --constraint=skylake
 
-export PATH="/project/surfadvisors/Software/ecoli-analysis-software/miniconda2/bin:$PATH"
-
-bash $HOME/ecoli-analysis-cephfs/run-variant-calling-cephfs-adv.sh 
